ecaf7ca6d22fcce6b88160db82e83431d74de5a3
Alex Zhuravlev <leftemail17@mail.ru>
2023-12-03 12:58:18 +0300

    feat: create bucket before start app

--- src/main/java/com/greenblat/cloudfilestorage/config/minio/MinioConfig.java
+++ src/main/java/com/greenblat/cloudfilestorage/config/minio/MinioConfig.java
@@ -1,9 +1,14 @@
 package com.greenblat.cloudfilestorage.config.minio;
 
+import com.greenblat.cloudfilestorage.exception.MinioOperationException;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,5 +22,28 @@ public class MinioConfig {
                 .endpoint(minioProperties.getUrl())
                 .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
                 .build();
+    }
+
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void createBucket() {
+        var minioClient = minioClient();
+        var bucketName = minioProperties.getBucket();
+        try {
+            var isFound = minioClient.bucketExists(
+                    BucketExistsArgs.builder()
+                            .bucket(bucketName)
+                            .build()
+            );
+            if (!isFound) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder()
+                                .bucket(bucketName)
+                                .build()
+                );
+            }
+        } catch (Exception e) {
+            throw new MinioOperationException("Bucket '" + bucketName + "' is not created");
+        }
     }
 }
